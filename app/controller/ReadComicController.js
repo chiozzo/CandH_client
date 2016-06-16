147a76f3c1e1546bc11bef41375dc83393247ad3
Martin Cross <mncross@gmail.com>
2016-06-16 15:58:33 -0500

    decode base64 and preview single comic

--- app/controller/ReadComicController.js
+++ app/controller/ReadComicController.js
@@ -9,8 +9,13 @@ app.controller('ReadComicController', [
 
     $scope.getComics = function() {
       $http
-      .get('http://localhost:5000/api/ComicStrip')
-      .success(gotComic => $scope.comic = gotComic);
-    }
+      .get('http://localhost:5000/api/ComicStrip?comicStripId=1')
+      .success(gotComic => {
+          $scope.comic = gotComic[0];
+          $scope.comic.Image = window.atob(gotComic[0].Image);
+      });
+    };
+
+    $scope.getComics();
   }
 ]);
