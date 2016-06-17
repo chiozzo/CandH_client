b08369a810e1441b28caabe30322cdef86a00d85
Martin Cross <mncross@gmail.com>
2016-06-17 09:37:55 -0500

    build get previous comic

--- app/controller/ReadComicController.js
+++ app/controller/ReadComicController.js
@@ -8,12 +8,12 @@ app.controller('ReadComicController', [
     $scope.comic = {};
 
     $scope.requestedComic = {
-      "Id": null,
+      "Id": 0,
       "getURL": 'http://localhost:5000/api/ComicStrip'
     };
 
     $scope.getComic = function() {
-      if ($scope.requestedComic.Id != null) {
+      if ($scope.requestedComic.Id != 0) {
         $scope.requestedComic.getURL = `http://localhost:5000/api/ComicStrip?comicStripId=${$scope.requestedComic.Id}`
       }
       console.log($scope.requestedComic.getURL);
@@ -33,12 +33,20 @@ app.controller('ReadComicController', [
     };
 
     $scope.getNextComic = function() {
-      if ($scope.requestedComic.Id == null) {
+      if ($scope.requestedComic.Id == 0) {
         $scope.requestedComic.Id = 1;
       }
       $scope.requestedComic.Id++;
       $scope.getComic();
     };
+
+    $scope.getPreviousComic = function() {
+      if ($scope.requestedComic.Id <= 2) {
+        $scope.requestedComic.Id = 2;
+      }
+      $scope.requestedComic.Id--;
+      $scope.getComic();
+    }
 
     $scope.getComic();
   }
