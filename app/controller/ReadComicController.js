f3ab096fcad8e54dbdae03c29fa2a510cf25efc8
Martin Cross <mncross@gmail.com>
2016-06-17 09:00:57 -0500

    begin work on next comic

--- app/controller/ReadComicController.js
+++ app/controller/ReadComicController.js
@@ -7,15 +7,39 @@ app.controller('ReadComicController', [
 
     $scope.comic = {};
 
-    $scope.getComics = function() {
-      $http
-      .get('http://localhost:5000/api/ComicStrip?comicStripId=1')
-      .success(gotComic => {
-          $scope.comic = gotComic[0];
-          $scope.comic.Image = window.atob(gotComic[0].Image);
-      });
+    $scope.requestedComic = {
+      "Id": null,
+      "getURL": 'http://localhost:5000/api/ComicStrip'
     };
 
-    $scope.getComics();
+    $scope.getComic = function() {
+      if ($scope.requestedComic.Id != null) {
+        $scope.requestedComic.getURL = `http://localhost:5000/api/ComicStrip?comicStripId=${$scope.requestedComic.Id}`
+      }
+      console.log($scope.requestedComic.getURL);
+      $http
+      .get($scope.requestedComic.getURL)
+      .then(
+        success => {
+          let gotComic = success.data[0];
+          $scope.comic = gotComic;
+          $scope.requestedComic.Id = gotComic.ComicStripId;
+          $scope.comic.Image = window.atob(gotComic.Image);
+        },
+        error => {
+          console.log(error);
+        }
+      );
+    };
+
+    $scope.getNextComic = function() {
+      if ($scope.requestedComic.Id == null) {
+        $scope.requestedComic.Id = 1;
+      }
+      $scope.requestedComic.Id++;
+      $scope.getComic();
+    };
+
+    $scope.getComic();
   }
 ]);
